27e9c18f8e4ee3512df4b97f2df7ff85736061e7
Agrotecnico <agrotecnicog@gmail.com>
2024-01-11 00:57:09 -0300

    Creating the project. Part 1.a

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import AgroLogo from '@/app/ui/agro-logo'
 import IconAgro from '@/app/ui/iconAgro'
-import LogoAgro from '@/app/ui/logoAgro'
+import LogoCNP from '@/app/ui/logoCNP'
 import { ArrowRightIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
 import styles from '@/app/ui/home.module.css'
@@ -10,15 +10,16 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-[#ff8fe7] p-4 md:h-52">
         {/* <AgroLogo /> */}
         
-        <LogoAgro className="h-[48px] w-[224px] "  color="white" />
+        <LogoCNP className="h-[32px] w-[48px] "  color="white" />
+        <div>Servicios</div>
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           {/* <div className={styles.shape }/> */}
-          <IconAgro className="h-[48px] w-[48px] mr-4 " />
+          <LogoCNP className="h-[32px] w-[48px] "  color="black" />
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
