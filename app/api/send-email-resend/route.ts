d835e805ba7ca12074eb22ac21b8993f792783aa
Agrotecnico <agrotecnicog@gmail.com>
2025-06-18 15:43:48 -0300

    actualizar 1

--- app/api/send-email-resend/route.ts
+++ app/api/send-email-resend/route.ts
@@ -1,22 +1,22 @@
-import { EmailTemplate } from "@/app/pruebas/components/email-template";
-import { NextResponse } from "next/server";
-import { Resend } from "resend";
+// import { EmailTemplate } from "@/app/pruebas/components/email-template";
+// import { NextResponse } from "next/server";
+// import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
-  try {
-    const data = await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
-      // to: ["agrotecnicog@gmail.com"],
-      to: ["jorge.szanto@agrotecnico.com.ar"],
-      subject: "Hello world",
-      react: EmailTemplate({ firstName: "John" }),
-      text: "",
-    });
+// export async function POST() {
+//   try {
+//     const data = await resend.emails.send({
+//       from: "Acme <onboarding@resend.dev>",
+//       // to: ["agrotecnicog@gmail.com"],
+//       to: ["jorge.szanto@agrotecnico.com.ar"],
+//       subject: "Hello world",
+//       react: EmailTemplate({ firstName: "John" }),
+//       text: "",
+//     });
 
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({ error });
-  }
-}
+//     return NextResponse.json(data);
+//   } catch (error) {
+//     return NextResponse.json({ error });
+//   }
+// }
